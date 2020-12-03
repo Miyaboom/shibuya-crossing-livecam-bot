43ad9ee1e8ce5c2940c09b6936ef8f6e2888b92a
宮分 匠 <takumi.miyabu@IBG-0035.local>
2020-12-04 02:05:52 +0900

    YoutubeのURLを変更

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://www.youtube.com/watch?v=21X5lGlDOfg');
+  await page.goto('https://www.youtube.com/watch?v=lkIJYc4UH60');
   await page.screenshot({ path: 'youtube.png' });
   await browser.close();
 })();
