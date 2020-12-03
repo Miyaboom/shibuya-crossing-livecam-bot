7a9bd38abafbb53c99ebc82ea3b8a11001fa9868
宮分 匠 <takumi.miyabu@IBG-0035.local>
2020-12-04 01:37:22 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.youtube.com/watch?v=21X5lGlDOfg');
+  await page.screenshot({ path: 'youtube.png' });
+  await browser.close();
+})();
