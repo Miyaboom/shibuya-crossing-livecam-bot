30ed282645d6d8f542e2df95a69471df4f0b82e5
宮分 匠 <takumi.miyabu@IBG-0035.local>
2020-12-04 01:49:42 +0900

    launchオプションを追加

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = __importDefault(require("puppeteer"));
 (async () => {
-    const browser = await puppeteer_1.default.launch();
+    const browser = await puppeteer_1.default.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://www.youtube.com/watch?v=21X5lGlDOfg');
     await page.screenshot({ path: 'youtube.png' });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('https://www.youtube.com/watch?v=21X5lGlDOfg');
   await page.screenshot({ path: 'youtube.png' });
