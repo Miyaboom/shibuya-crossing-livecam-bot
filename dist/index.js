e2b150acc9208fb88bdb5c530d7bed06ec1bd279
宮分 匠 <takumi.miyabu@IBG-0035.local>
2020-12-05 00:58:08 +0900

    スクリーンショットを確認するためのwebサーバーを実装

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const express_1 = __importDefault(require("express"));
 (async () => {
-    const browser = await puppeteer_1.default.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer_1.default.launch({
+        executablePath: '/app/.apt/usr/bin/google-chrome',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
-    await page.goto('https://www.youtube.com/watch?v=21X5lGlDOfg');
-    await page.screenshot({ path: 'youtube.png' });
+    await page.goto('https://www.youtube.com/watch?v=lkIJYc4UH60');
+    await page.screenshot({ path: 'public/youtube.png' });
     await browser.close();
+    const app = express_1.default();
+    const port = 3000;
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+    app.use(express_1.default.static('public'));
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import puppeteer from 'puppeteer';
+import express from 'express'
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    executablePath: '/app/.apt/usr/bin/google-chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('https://www.youtube.com/watch?v=lkIJYc4UH60');
-  await page.screenshot({ path: 'youtube.png' });
+  await page.screenshot({ path: 'public/youtube.png' });
   await browser.close();
+
+  const app = express()
+  const port = 3000
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.use(express.static('public'));
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
 })();
