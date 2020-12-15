b7b04ddcde0200fb5b366f25b12ad9518f5555c7
宮分 匠 <takumi.miyabu@IBG-0035.local>
2020-12-16 01:50:59 +0900

    ローカル環境用の処理をやめる

--- dist/index.js
+++ dist/index.js
@@ -10,17 +10,12 @@ const fs_1 = __importDefault(require("fs"));
     // 環境変数
     const chromePath = process.env.CHROME_PATH || '';
     const youtubePath = process.env.YOUTUBE_PATH || '';
-    const youtubeChatPath = process.env.YOUTUBE_CHAT_PATH || '';
     const twitterConsumerKey = process.env.TWITTER_CONSUMER_KEY || '';
     const twitterConsumerSecret = process.env.TWITTER_CONSUMER_SECRET || '';
     const twitterTokenKey = process.env.TWITTER_TOKEN_KEY || '';
     const twitterTokenSecret = process.env.TWITTER_TOKEN_SECRET || '';
-    // コマンドライン引数
-    const args = process.argv.slice(2);
-    const takeChatScreenShot = args[0] || ''; // 引数有りで実行した場合チャット欄もスクショする
     // Puppeteer
     const browser = await puppeteer_1.default.launch({
-        headless: takeChatScreenShot ? false : true,
         executablePath: chromePath,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
@@ -30,17 +25,12 @@ const fs_1 = __importDefault(require("fs"));
     });
     const playButtonSelector = 'button.ytp-large-play-button.ytp-button';
     const screenshotPath = 'public/images/screenshot.png';
-    const chatScreenShotPath = 'public/images/chat_screenshot.png';
+    const waitTime = 180000; // 広告の終了を待機
     await page.goto(youtubePath);
     await page.waitForSelector(playButtonSelector);
     await page.click(playButtonSelector);
-    await page.waitForTimeout(180000);
+    await page.waitForTimeout(waitTime);
     await page.screenshot({ path: screenshotPath });
-    if (takeChatScreenShot) {
-        await page.goto(youtubeChatPath);
-        await page.waitForTimeout(1000);
-        await page.screenshot({ path: chatScreenShotPath });
-    }
     await browser.close();
     // Twitter
     const twitterClient = new twitter_1.default({
@@ -55,22 +45,12 @@ const fs_1 = __importDefault(require("fs"));
             return fs_1.default.readFileSync(screenshotPath);
         }
         catch (error) {
-            console.error(`failed to read ${error}`);
+            console.error('failed to read ', error);
         }
     })();
     const media = await twitterClient.post('media/upload', { media: data });
-    // ライブチャットのスクリーンショットをTwitterにアップロード
-    const chatdata = takeChatScreenShot ? (() => {
-        try {
-            return fs_1.default.readFileSync(chatScreenShotPath);
-        }
-        catch (error) {
-            console.error(`failed to read ${error}`);
-        }
-    })() : null;
-    const chatmedia = takeChatScreenShot && chatdata ? await twitterClient.post('media/upload', { media: chatdata }) : null;
     // Twitterに投稿
-    const media_ids = takeChatScreenShot && chatmedia ? `${media.media_id_string},${chatmedia.media_id_string}` : `${media.media_id_string}`;
+    const media_ids = `${media.media_id_string}`;
     twitterClient.post('statuses/update', {
         status: '#渋谷 #渋谷スクランブル交差点 #Shibuya #ShibuyaCrossing',
         media_ids: media_ids
--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,13 @@ import fs from 'fs'
   // 環境変数
   const chromePath = process.env.CHROME_PATH || ''
   const youtubePath = process.env.YOUTUBE_PATH || ''
-  const youtubeChatPath = process.env.YOUTUBE_CHAT_PATH || ''
   const twitterConsumerKey = process.env.TWITTER_CONSUMER_KEY || ''
   const twitterConsumerSecret = process.env.TWITTER_CONSUMER_SECRET || ''
   const twitterTokenKey = process.env.TWITTER_TOKEN_KEY || ''
   const twitterTokenSecret = process.env.TWITTER_TOKEN_SECRET || ''
 
-  // コマンドライン引数
-  const args = process.argv.slice(2);
-  const takeChatScreenShot = args[0] || '' // 引数有りで実行した場合チャット欄もスクショする
-
   // Puppeteer
   const browser = await puppeteer.launch({
-    headless: takeChatScreenShot ? false : true, // チャット欄をスクショする場合ヘッドフルモードで起動する
     executablePath: chromePath,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
@@ -31,20 +25,13 @@ import fs from 'fs'
 
   const playButtonSelector = 'button.ytp-large-play-button.ytp-button'
   const screenshotPath = 'public/images/screenshot.png'
-  const chatScreenShotPath = 'public/images/chat_screenshot.png'
+  const waitTime = 180000 // 広告の終了を待機
 
   await page.goto(youtubePath);
   await page.waitForSelector(playButtonSelector);
   await page.click(playButtonSelector);
-  await page.waitForTimeout(180000); // 広告の終了を待機
+  await page.waitForTimeout(waitTime);
   await page.screenshot({ path: screenshotPath });
-
-  if (takeChatScreenShot) {
-    await page.goto(youtubeChatPath);
-    await page.waitForTimeout(1000);
-    await page.screenshot({ path: chatScreenShotPath });
-  }
-
   await browser.close();
 
   // Twitter
@@ -60,23 +47,13 @@ import fs from 'fs'
     try {
       return fs.readFileSync(screenshotPath);
     } catch (error) {
-      console.error(`failed to read ${error}`)
+      console.error('failed to read ', error)
     }
   })();
   const media = await twitterClient.post('media/upload', { media: data });
 
-  // ライブチャットのスクリーンショットをTwitterにアップロード
-  const chatdata = takeChatScreenShot ? (() => {
-    try {
-      return fs.readFileSync(chatScreenShotPath);
-    } catch (error) {
-      console.error(`failed to read ${error}`)
-    }
-  })() : null
-  const chatmedia = takeChatScreenShot && chatdata ? await twitterClient.post('media/upload', { media: chatdata }) : null;
-
   // Twitterに投稿
-  const media_ids = takeChatScreenShot && chatmedia ? `${media.media_id_string},${chatmedia.media_id_string}` : `${media.media_id_string}`
+  const media_ids = `${media.media_id_string}`
 
   twitterClient.post('statuses/update', {
     status: '#渋谷 #渋谷スクランブル交差点 #Shibuya #ShibuyaCrossing',
